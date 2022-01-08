27acad74bbf5dd2c3c9dc8ce402258623e94994c
TejasHackathons <tejashackathons@gmail.com>
2022-01-07 23:55:17 -0800

    Almost done with backend. All that's left is the ML model and then frontend

--- backend/Routes/disasterRoutes.js
+++ backend/Routes/disasterRoutes.js
@@ -8,9 +8,7 @@ const latLongValidator = require("../Validators/validLatLong");
 router.post("/createDisaster", async (req, res) => {
   if (!req.session.userID)
     return res.status(401).send("Must be logged in to report disaster. ");
-
   const { disaster, currentLat, currentLong } = req.body;
-  console.log(disaster);
   if (!disasterValidator(disaster))
     return res.status(400).send("Invalid disaster type. ");
   if (!latLongValidator(currentLat, currentLong))
@@ -25,4 +23,22 @@ router.post("/createDisaster", async (req, res) => {
   return res.status(201).send("Successfully reported disaster. ");
 });
 
+router.get("/disasterList", async (req, res) => {
+  const userID = req.session.userID;
+  if (!userID)
+    return res.status(401).send("Must be logged in to view disasters. ");
+  const { data, error } = await supabase.from("disasters").select();
+  if (error) return res.status(500).send("Internal server error. ");
+  return res.json({ msg: data });
+});
+
+router.get("/disasterInfo", async (req, res) => {
+  const { disasterID } = req.body;
+  const { data, error } = await supabase
+    .from("disasters")
+    .select()
+    .eq("id", disasterID);
+  if (error) return res.status(500).send("Internal server error. ");
+  return res.json({ msg: data[0] });
+});
 module.exports = router;
